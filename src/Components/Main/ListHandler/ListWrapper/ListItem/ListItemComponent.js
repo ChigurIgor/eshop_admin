ed53759709c1d798cb60ad281d4398294de7e89b
ChigurIgor <chigur.ig@gmail.com>
2022-02-02 20:39:19 +0200

    Started work on components

--- src/Components/Main/ListHandler/ListWrapper/ListItem/ListItemComponent.js
+++ src/Components/Main/ListHandler/ListWrapper/ListItem/ListItemComponent.js
@@ -1,10 +1,23 @@
 import styles from './styles.module.css';
 
 const ListItemComponent = () =>{
-    const {wrapper} = styles;
+    const {wrapper, image, subwrapper, title, description, button} = styles;
     return(
         <div className={wrapper}>
-            List item
+            <img
+                className={image}
+                src={'https://litb-cgis.rightinthebox.com/images/640x853/202109/bps/product/inc/fmmega1632724389946.jpg?fmt=webp&v=1'}
+            />
+            <div className={subwrapper}>
+                <h3 className={title}>Title</h3>
+                <p className={description}>Description</p>
+            </div>
+            <button
+                className={button}
+                onClick={()=>{}}
+            >
+                Delete
+            </button>
         </div>
     )
 }
